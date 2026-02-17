af4c2c88c68657f03839129fc1a621945b2f58f5
NIGHTZIN <hatsunemikutoro@gmail.com>
2026-02-17 11:20:02 -0300

    First Commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const nameIn = document.getElementById("nameIn");
+const passIn = document.getElementById("passIn");
+const output = document.getElementById("output");
+
+const btn = document.getElementById("subm");
+
+btn.addEventListener("click", () => {
+    if (nameIn.value && passIn.value) {
+        if (nameIn.value === "githubmidnight1" && passIn.value === "rockyoutxt")
+        {
+            window.alert("LOGIN");
+        } else {
+            output.textContent = "Senha ou login incorretos."
+        }
+    } else {
+        output.textContent = "É necessário preencher os campos."
+    }
+})
