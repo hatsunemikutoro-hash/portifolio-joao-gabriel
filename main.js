aabc6cfc6c46e730b3fea79120652a60f777885b
NIGHTZIN <hatsunemikutoro@gmail.com>
2026-02-17 11:53:54 -0300

    novas alterações no site

--- main.js
+++ main.js
@@ -6,13 +6,17 @@ const btn = document.getElementById("subm");
 
 btn.addEventListener("click", () => {
     if (nameIn.value && passIn.value) {
+
         if (nameIn.value === "githubmidnight1" && passIn.value === "rockyoutxt")
         {
             window.alert("LOGIN");
-        } else {
+        } 
+        else {
             output.textContent = "Senha ou login incorretos."
         }
-    } else {
+    } else if (nameIn.value === "midnight1") {
+            output.textContent = "aonde foi que você viu isso?"
+        } else {
         output.textContent = "É necessário preencher os campos."
     }
 })
